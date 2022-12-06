40c27352fcd672b66450afa7eb3c160c0c8fd30a
Zhe Song <zhs064@gmail.com>
2022-12-06 11:35:04 -0700

    Level 3 - Hashing Passwords

--- app.js
+++ app.js
@@ -4,14 +4,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
+
 main().catch(err => console.log(err));
 
 
 async function main() {
-  const app = express();
-
-  //console.log(process.env.API_KEY);
+  const app = express();  
 
   app.use(express.static("public"));
   app.set('view engine', 'ejs');
@@ -27,8 +26,7 @@ async function main() {
     password: String
   });
 
-  const secret = process.env.SECRET;
-  userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
+
 
   const User = new mongoose.model('User', userSchema);
 
@@ -48,7 +46,7 @@ async function main() {
   app.post("/register", function(req, res) {
     const newUser = new User({
       email: req.body.username,
-      password: req.body.password
+      password: md5(req.body.password)
     });
 
     newUser.save(function(err) {
@@ -62,7 +60,7 @@ async function main() {
 
   app.post("/login", function(req, res) {
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({email: username}, function(err, foundUser) {
       if (err) {
